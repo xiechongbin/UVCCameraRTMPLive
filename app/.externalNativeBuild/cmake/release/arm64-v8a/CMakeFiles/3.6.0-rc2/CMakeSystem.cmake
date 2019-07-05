8538dac14411315ba6a6b9f28e6e37868e7d1188
YoungWu <yinyw@gzyitop.com>
2019-07-05 15:05:21 +0800

    remove some ignored files.

--- app/.externalNativeBuild/cmake/release/arm64-v8a/CMakeFiles/3.6.0-rc2/CMakeSystem.cmake
+++ app/.externalNativeBuild/cmake/release/arm64-v8a/CMakeFiles/3.6.0-rc2/CMakeSystem.cmake
@@ -3,11 +3,11 @@ set(CMAKE_HOST_SYSTEM_NAME "Darwin")
 set(CMAKE_HOST_SYSTEM_VERSION "16.7.0")
 set(CMAKE_HOST_SYSTEM_PROCESSOR "x86_64")
 
-include("/Users/cxy/work/android-sdks/android-ndk-r16b/build/cmake/android.toolchain.cmake")
+include("/Library/Android/sdk/ndk-bundle/build/cmake/android.toolchain.cmake")
 
-set(CMAKE_SYSTEM "Android-1")
+set(CMAKE_SYSTEM "Android-21")
 set(CMAKE_SYSTEM_NAME "Android")
-set(CMAKE_SYSTEM_VERSION "1")
+set(CMAKE_SYSTEM_VERSION "21")
 set(CMAKE_SYSTEM_PROCESSOR "aarch64")
 
 set(CMAKE_CROSSCOMPILING "TRUE")
--- app/.externalNativeBuild/cmake/release/x86_64/cmake_install.cmake
+++ app/.externalNativeBuild/cmake/release/x86_64/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/cxy/work/projects/cxy_project_hs/UVCCameraRTMPLive/app
+# Install script for directory: /Users/YoungWu/StudioProjects/UVCCameraRTMPLive/app
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -40,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/Users/cxy/work/projects/cxy_project_hs/UVCCameraRTMPLive/app/.externalNativeBuild/cmake/release/x86_64/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/Users/YoungWu/StudioProjects/UVCCameraRTMPLive/app/.externalNativeBuild/cmake/release/x86_64/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
